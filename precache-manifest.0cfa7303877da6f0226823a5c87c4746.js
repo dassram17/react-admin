28ec5798d9c80104d7800a43015c4b43b4ff4b50
unknown <dassram17@gmail.com>
2020-08-19 11:45:23 +0530

    Updates

---
+++ precache-manifest.0cfa7303877da6f0226823a5c87c4746.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "346ab69cbfd9c74d459fd220dc43f9ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d43aa8f17689fbe1239d",
+    "url": "/static/css/2.90b4b531.chunk.css"
+  },
+  {
+    "revision": "932e5059b8579b93c2f4",
+    "url": "/static/css/main.4d4a8ca2.chunk.css"
+  },
+  {
+    "revision": "d43aa8f17689fbe1239d",
+    "url": "/static/js/2.f0efb4f8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f0efb4f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "932e5059b8579b93c2f4",
+    "url": "/static/js/main.ecd62d42.chunk.js"
+  },
+  {
+    "revision": "ca47be02ee6e664cd44d",
+    "url": "/static/js/runtime-main.a1318aaf.js"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "2a103029dda6ab1bc9c8b0ee72536e41",
+    "url": "/static/media/Simple-Line-Icons.2a103029.svg"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/static/media/Simple-Line-Icons.f33df365.eot"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
