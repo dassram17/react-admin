3d924831ca61c02fd3026284674ecbf1c1507eb7
unknown <dassram17@gmail.com>
2020-08-30 15:40:37 +0530

    commit

---
+++ public/precache-manifest.dc607fe156f9e77203cc0bb887ec95e5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c538af2791a709f40a3bc92f80546d53",
+    "url": "/react-admin/index.html"
+  },
+  {
+    "revision": "c04b0574ad6160128624",
+    "url": "/react-admin/static/css/2.30fbf3a2.chunk.css"
+  },
+  {
+    "revision": "e8eedec63dcdb7eb8f95",
+    "url": "/react-admin/static/css/main.386cb54e.chunk.css"
+  },
+  {
+    "revision": "c04b0574ad6160128624",
+    "url": "/react-admin/static/js/2.7b96f255.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-admin/static/js/2.7b96f255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8eedec63dcdb7eb8f95",
+    "url": "/react-admin/static/js/main.dd9e80aa.chunk.js"
+  },
+  {
+    "revision": "123b9cabd9cba0d490c2",
+    "url": "/react-admin/static/js/runtime-main.9a5ddb83.js"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/react-admin/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "2a103029dda6ab1bc9c8b0ee72536e41",
+    "url": "/react-admin/static/media/Simple-Line-Icons.2a103029.svg"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/react-admin/static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/react-admin/static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/react-admin/static/media/Simple-Line-Icons.f33df365.eot"
+  },
+  {
+    "revision": "04a9f16617a66a019626f89d3ac11aa6",
+    "url": "/react-admin/static/media/doctor_toon.04a9f166.png"
+  },
+  {
+    "revision": "3d4fcbd364e6f9505dce771a95f3e1d0",
+    "url": "/react-admin/static/media/login-side.3d4fcbd3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-admin/static/media/logo.5d5d9eef.svg"
+  }
+]);
